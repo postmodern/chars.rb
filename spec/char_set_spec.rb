bc28b59da07c8310abb15cb9d61cacc30e86ffb9
Postmodern <postmodern.mod3@gmail.com>
2012-07-10 21:34:28 -0700

    Group specs together.

--- spec/char_set_spec.rb
+++ spec/char_set_spec.rb
@@ -87,73 +87,93 @@ describe Chars::CharSet do
     }.should == true
   end
 
-  it "should return a random Array of bytes" do
-    bytes = subject.random_bytes(10)
+  describe "#random_bytes" do
+    it "should return a random Array of bytes" do
+      bytes = subject.random_bytes(10)
 
-    bytes.all? { |b| subject.include?(b) }.should == true
+      bytes.all? { |b| subject.include?(b) }.should == true
+    end
+
+    context "with a range of lengths" do
+      it "should return a random Array of bytes with a varying length" do
+        bytes = subject.random_bytes(5..10)
+
+        bytes.length.should be_between(5, 10)
+        bytes.all? { |b| subject.include?(b) }.should == true
+      end
+    end
   end
 
-  it "should return a random Array of chars" do
-    chars = subject.random_chars(10)
+  describe "#random_chars" do
+    it "should return a random Array of chars" do
+      chars = subject.random_chars(10)
 
-    chars.all? { |c| subject.include_char?(c) }.should == true
+      chars.all? { |c| subject.include_char?(c) }.should == true
+    end
+
+    context "with a range of lengths" do
+      it "should return a random Array of chars with a varying length" do
+        chars = subject.random_chars(5..10)
+
+        chars.length.should be_between(5, 10)
+        chars.all? { |c| subject.include_char?(c) }.should == true
+      end
+    end
   end
 
-  it "should return a random Array of bytes with a varying length" do
-    bytes = subject.random_bytes(5..10)
+  describe "#random_string" do
+    it "should return a random String of chars" do
+      string = subject.random_string(10)
 
-    bytes.length.should be_between(5, 10)
-    bytes.all? { |b| subject.include?(b) }.should == true
-  end
+      string.chars.all? { |b| subject.include_char?(b) }.should == true
+    end
 
-  it "should return a random Array of chars with a varying length" do
-    chars = subject.random_chars(5..10)
+    context "with a range of lengths" do
+      it "should return a random String of chars with a varying length" do
+        string = subject.random_string(5..10)
 
-    chars.length.should be_between(5, 10)
-    chars.all? { |c| subject.include_char?(c) }.should == true
-  end
-
-  it "should return a random String of chars" do
-    string = subject.random_string(10)
-    
-    string.chars.all? { |b| subject.include_char?(b) }.should == true
-  end
-
-  it "should return a random String of chars with a varying length" do
-    string = subject.random_string(5..10)
-
-    string.length.should be_between(5, 10)
-    string.chars.all? { |b| subject.include_char?(b) }.should == true
+        string.length.should be_between(5, 10)
+        string.chars.all? { |b| subject.include_char?(b) }.should == true
+      end
+    end
   end
   
-  it "should return a random Array of unique bytes" do
-    bytes = subject.random_distinct_bytes(10)
+  describe "#random_distinct_bytes" do
+    it "should return a random Array of unique bytes" do
+      bytes = subject.random_distinct_bytes(10)
 
-    bytes.uniq.should == bytes
-    bytes.all? { |b| subject.include?(b) }.should == true
+      bytes.uniq.should == bytes
+      bytes.all? { |b| subject.include?(b) }.should == true
+    end
+
+    context "with a range of lengths" do
+      it "should return a random Array of unique bytes with a varying length" do
+        bytes = subject.random_distinct_bytes(5..10)
+
+        bytes.uniq.should == bytes
+        bytes.length.should be_between(5, 10)
+        bytes.all? { |b| subject.include?(b) }.should == true
+      end
+    end
   end
 
-  it "should return a random Array of unique chars" do
-    chars = subject.random_distinct_chars(10)
+  describe "#random_distinct_chars" do
+    it "should return a random Array of unique chars" do
+      chars = subject.random_distinct_chars(10)
 
-    chars.uniq.should == chars
-    chars.all? { |c| subject.include_char?(c) }.should == true
-  end
+      chars.uniq.should == chars
+      chars.all? { |c| subject.include_char?(c) }.should == true
+    end
 
-  it "should return a random Array of unique bytes with a varying length" do
-    bytes = subject.random_distinct_bytes(5..10)
+    context "with a range of lengths" do
+      it "should return a random Array of unique chars with a varying length" do
+        chars = subject.random_distinct_chars(5..10)
 
-    bytes.uniq.should == bytes
-    bytes.length.should be_between(5, 10)
-    bytes.all? { |b| subject.include?(b) }.should == true
-  end
-
-  it "should return a random Array of unique chars with a varying length" do
-    chars = subject.random_distinct_chars(5..10)
-
-    chars.uniq.should == chars
-    chars.length.should be_between(5, 10)
-    chars.all? { |c| subject.include_char?(c) }.should == true
+        chars.uniq.should == chars
+        chars.length.should be_between(5, 10)
+        chars.all? { |c| subject.include_char?(c) }.should == true
+      end
+    end
   end
 
   it "should be able to be compared with another set of chars" do
@@ -174,15 +194,17 @@ describe Chars::CharSet do
     sub_set.should be_subset(subject)
   end
 
-  it "should find one sub-string from a String belonging to the char set" do
-    subject.strings_in("AAAA").should == ["AAAA"]
-  end
+  describe "#strings_in" do
+    it "should find one sub-string from a String belonging to the char set" do
+      subject.strings_in("AAAA").should == ["AAAA"]
+    end
 
-  it "should find sub-strings from a String belonging to the char set" do
-    subject.strings_in("AAAA!B!CCCCCC").should == [
-      "AAAA",
-      "CCCCCC"
-    ]
+    it "should find sub-strings from a String belonging to the char set" do
+      subject.strings_in("AAAA!B!CCCCCC").should == [
+        "AAAA",
+        "CCCCCC"
+      ]
+    end
   end
 
   it "should determine if a String is made up of the characters from the char set" do
